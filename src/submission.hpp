540adb191e84db9629a6cfe53f7264db22848269
Nicholas-py <nicholas.waslander@gmail.com>
2026-09-16 15:08:59 -0400

    Cleanup

--- src/submission.hpp
+++ src/submission.hpp
@@ -12,10 +12,10 @@
 class Grid
 {
 private:
-  std::size_t rows_;
-  std::size_t cols_;
-  std::size_t maxrow_;
-  std::size_t maxcol_;
+  const std::size_t rows_;
+  const std::size_t cols_;
+  const std::size_t maxrow_;
+  const std::size_t maxcol_;
 
 public:
   Grid(std::size_t rows, std::size_t cols);
@@ -38,24 +38,19 @@ public:
     return maxcol_;
   }
 
-  void setrowcol(std::size_t rows, std::size_t cols) {
-    rows_ = rows;
-    cols_ = cols;
-    maxrow_ = rows-1;
-    maxcol_ = cols-1;
-  }
 };
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
 void apply_stencil(const Grid &old_grid, Grid &new_grid);
 
-Grid::Grid(std::size_t rows, std::size_t cols) {
-  setrowcol(rows, cols);
-  
-  std::vector<double> data(rows*cols , 0);
-  this->array = data;
-}
+Grid::Grid(std::size_t rows, std::size_t cols)
+:  rows_(rows), 
+  cols_(cols), 
+  maxrow_(rows-1), 
+  maxcol_(cols-1), 
+  array(rows*cols, 0)
+  {  }
 
 inline double& Grid::operator()(std::size_t i, std::size_t j) {
   return this->array[i*cols_+j];
@@ -66,9 +61,12 @@ inline double Grid::operator()(std::size_t i, std::size_t j) const {
 }
 
 inline void apply_stencil(const Grid &old_grid, Grid &new_grid) {
+
   #pragma omp parallel for
-  for (std::size_t i = 1; i < (old_grid.maxrow()); i++) {
+  for (std::size_t i = 1; i < old_grid.maxrow(); i++) {
+    
     std::size_t i2 = i*old_grid.cols();
+
     for (std::size_t j = 1; j < old_grid.maxcol(); j++) {
       new_grid.array[i2+j] = 0.5*(old_grid.array[i2+j]) +0.125*(old_grid.array[i2+j-1] + old_grid.array[i2+j+1] + old_grid.array[i2+j+old_grid.cols()] + old_grid.array[i2+j-old_grid.cols()]);
     }
